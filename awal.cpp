2f1ba1012b258555cb778819cab0b67e44f55d58
Joseph Wellington Robinson R <127705735+JosephCETH30@users.noreply.github.com>
2023-04-06 11:46:10 +0700

    update

---
+++ awal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	short angka1 = 80;
+	int angka2 = 50000;
+	long angka3 = 2000000000; 
+	float angka4 = 0.75;
+	double angka5 = 0.000078;
+	char karakter = 'A';
+	bool logika = true;
+	string nama = "Joseph R.";
+	
+	cout << angka1 << endl;
+	cout << angka2 << endl;
+	cout << angka3 << endl;
+	cout << angka4 << endl;
+	cout << angka5 << endl;
+	cout << karakter << endl;
+	cout << logika << endl;
+	cout << nama << endl;
+	
+	return 0;
+}
